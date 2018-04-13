075e4ad6ad991a5258656c39285a7cb208d66db5
Rose <hey_cool@163.com>
2018-04-13 18:38:56 +0800

    尝试

--- pages/me/me.js
+++ pages/me/me.js
@@ -1,14 +1,41 @@
 //logs.js
 const util = require('../../utils/util.js')
-
 Page({
-  data: {
-    logs: []
-  },
-  onLoad: function () {
-    this.setData({
-      Rose: 'my name is Rose Zhao'
+    data: {
+        count: 1,
+        staffA: { firstName: 'Hulk', lastName: 'Hu' },
+        staffB: { firstName: 'Shang', lastName: 'You' },
+        staffC: { firstName: 'Gideon', lastName: 'Lin' },
 
-    })
-  }
+        object: {
+            key: 'Hello '
+        },
+        array: ['MINA'],
+        zero: 0,
+         obj1: {
+          a: 1,
+          b: 2
+        },
+        obj2: {
+          c: 3,
+          d: 4
+        },
+        item:{
+            index: 0,
+            msg: 'this id Rose',
+            time: new Date().toLocaleDateString()
+        }
+    },
+
+    add: function(e) {
+        this.setData({
+            count: this.data.count + 1
+        })
+    },
+    onLoad: function() {
+        this.setData({
+            Rose: 'my name is Rose Zhao',
+            arr: [1, 2, 3, 4, 5]
+        })
+    }
 })
