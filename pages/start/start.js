5bf669ca2b13c6ed072869d8561c25b799c7b050
Rose <hey_cool@163.com>
2018-07-31 18:53:27 +0800

    添加引导页

--- pages/start/start.js
+++ pages/start/start.js
@@ -13,15 +13,7 @@ Page({
     });
   },
   onLoad:function(){
-    var that = this
-    wx.setNavigationBarTitle({
-      title: wx.getStorageSync('mallName')
-    })
-    app.getUserInfo(function(userInfo){
-      that.setData({
-        userInfo: userInfo
-      })
-    })
+
   },
   onShow:function(){
 
@@ -34,6 +26,7 @@ Page({
       });
     }, 1000);
     wx.onAccelerometerChange(function(res) {
+
       var angle = -(res.x*30).toFixed(1);
       if(angle>14){ angle=14; }
       else if(angle<-14){ angle=-14; }
