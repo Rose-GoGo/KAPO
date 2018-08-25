938215f6b5263c057fee20544fe5dfba1f8835b2
Rose <hey_cool@163.com>
2018-08-25 15:43:14 +0800

    上传图片并且预览

--- pages/more/more.js
+++ pages/more/more.js
@@ -277,11 +277,21 @@ Page({
     
     
   },
+
+  handleImagePreview(e) {
+    const idx = e.target.dataset.idx
+    const images = this.data.images
+    wx.previewImage({
+      current: images[idx],  //当前预览的图片
+      urls: images,  //所有要预览的图片
+    })
+  },
+
   uploadImg: function(){
     var that = this;
     wx.chooseImage({
       //count: 1, // 默认9
-      sizeType: ['compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
       sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
       success: function (res) {
         // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
@@ -302,4 +312,5 @@ Page({
     })
 
   }
+ 
 })
