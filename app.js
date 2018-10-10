01114b1c622bee809e112a20d02015da7d4d2ff4
Rose <hey_cool@163.com>
2018-10-10 13:55:46 +0800

    修改文章列表的排序

--- app.js
+++ app.js
@@ -1,25 +1,31 @@
 //app.js
 App({
-  onLoad: function (options) {
-  },
-  onShow: function (option) {
-  },
-  onPageNotFound: function (res) {
-  },
-  onLaunch: function () {
+  onLoad: function(options) {},
+  onShow: function(option) {},
+  onPageNotFound: function(res) {},
+  onLaunch: function() {
     wx.login({
-      success: function (res) {
-        if (res.code) {
-
-        }
+      success: function(res) {
+        if (res.code) {}
       }
     })
   },
   globalData: {
-    kinds: [
-      { icon: 'icon-quanju', name: '前端', catid: '11' },
-      { icon: 'icon-anquan', name: '安全', catid: '12' },
-      { icon: 'icon-xiazai47', name: '生活', catid: '13' }
+    kinds: [{
+        icon: 'icon-xiazai47',
+        name: '生活',
+        catid: '13'
+      },
+      {
+        icon: 'icon-anquan',
+        name: '安全',
+        catid: '12'
+      },
+      {
+        icon: 'icon-quanju',
+        name: '前端',
+        catid: '11'
+      }
     ],
     userInfo: {}
   }
