30f2c67952c567cb14515459d6a704085cd54d0a
Rose <hey_cool@163.com>
2018-10-18 20:41:01 +0800

    首页下拉刷新loading去除

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,7 +5,7 @@ Page({
   /**
    * 页面的初始数据
    */
-   data: {
+  data: {
     notices: [],
     items: [],
     top10: [],
@@ -16,7 +16,7 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-   onLoad: function (options) {
+  onLoad: function (options) {
     var that = this;
     that.getNotice();
     that.top10();
@@ -25,32 +25,30 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-   onReady: function () { },
+  onReady: function () { },
   /**
    * 生命周期函数--监听页面显示
    */
-   onShow: function () { },
+  onShow: function () { },
   /**
    * 生命周期函数--监听页面隐藏
    */
-   onHide: function () { },
+  onHide: function () { },
   /**
    * 生命周期函数--监听页面卸载
    */
-   onUnload: function () { },
+  onUnload: function () { },
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-   onPullDownRefresh: function () {
-    wx.showLoading();
+  onPullDownRefresh: function () {
     let that = this;
     that.setData({
       items: [],
       page: 1,
       loadMore: true
     });
-    if (that.data.loadMore) {
-      wx.showLoading();
+    if (that.data.loadMore) {    
       that.top10();
       that.getLists();
     }
@@ -58,7 +56,7 @@ Page({
   /**
    * 页面上拉触底事件的处理函数
    */
-   onReachBottom: function () {
+  onReachBottom: function () {
     let that = this;
     let _page = that.data.page + 1;
     that.setData({
@@ -71,7 +69,7 @@ Page({
   /**
    * 用户点击右上角分享
    */
-   onShareAppMessage: function () {
+  onShareAppMessage: function () {
     return {
       title: '锲而舍之,朽木不折;锲而不舍,金石可镂',
       imageUrl: '/assets/images/share.jpg'
@@ -101,7 +99,7 @@ Page({
         that.setData({
           items: _items
         });
-  
+
       }
     });
   },
