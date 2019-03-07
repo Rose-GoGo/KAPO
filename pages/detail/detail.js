3329846bb62f1fb5a6c5c1d09046ce49cb42b2f5
Rose <hey_cool@163.com>
2019-03-07 19:29:48 +0800

    准备修改

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -34,6 +34,7 @@ Page({
      * 生命周期函数--监听页面加载
      */
      onLoad: function(options) {
+        console.log(options)
         wx.showLoading();
         var that = this;
         let _userInfo = wx.getStorageSync('userInfo')
