8a8668513abeb9a455cc187e9d5dfb07da26e136
Rose <hey_cool@163.com>
2019-06-14 10:08:08 +0800

    修改我

---
+++ components/copyright/copyright.js
@@ -0,0 +1,34 @@
+// components/bottombar/bottombar.js
+// import Api from '/../../utils/api.js';
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+       
+    },
+    /**
+     * 组件的初始数据
+     */
+    data: {
+      yearss: '2019'
+    },
+    ready: function(){
+      this.getYear();
+
+    },
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+        getYear: function() {
+            var date = new Date();
+            var _year = date.getFullYear();
+            console.log(_year)
+            // return year;
+            this.setData({
+              yearss: _year
+            })
+        }
+    }
+})
